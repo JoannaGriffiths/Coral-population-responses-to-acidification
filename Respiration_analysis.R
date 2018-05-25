c94a5865c1699030c706af4ff62055f56bc1df13
Joanna S. Griffiths <joanna.griffiths4@gmail.com>
2018-05-25 17:23:52 -0500

    Update Respiration_analysis.R

--- Respiration_analysis.R
+++ Respiration_analysis.R
@@ -1,56 +1,37 @@
 setwd("~/Desktop/Griffiths2018_data")
 
+
 library(lme4)
+library(car)
+library(emmeans)
 
-Resp1=read.table("Resp_day9_manuscript.csv", header = TRUE) #read in data
-str(Resp1) #quickly view data
-head(Resp1)
+Resp14=read.table("Resp1-4_umol_v2.csv", header = TRUE)
+Resp14
 
-#making the Jar and Coral numbers facotrs and not integers
-Resp1$Jar <- as.factor(Resp1$Jar)
-Resp1$Coral <- as.factor(Resp1$Coral)
-str(Resp1)
-head(Resp1)
+Resp14$Jar <- as.factor(Resp14$Jar)
+Resp14$Coral <- as.factor(Resp14$Coral)
+Resp14$Week <- as.factor(Resp14$Week)
+attach(Resp14)
+str(Resp14)
+head(Resp14)
 
-#normalizaing the data
-attach(Resp1)
-abResp1=abs(umolghr)
-abResp1
-lgResp1=log(abResp1)
-lgResp1
+abResp14=abs(Resp14$umolghr) #normalizing data
+abResp14
+Resp14$lgResp14=log(abResp14) 
+lgResp14 = log(abResp14)
 
-hist(lgResp1, main="Histogram of Respiration Week 1")
-shapiro.test(lgResp1)
+Resp4HU_L <- c(-0.50080676, -1.24811931, -1.20419333, -1.4574839, -0.9196009, -0.60189205, -0.08969823, -1.27003099, -0.74468992)
+Resp4HU_H <- c(0.64724495, 0.85766221, -0.8422587, -1.25309985, -0.75145199, -0.8163816, -1.31263915, -0.98143827, -1.06324171)
+Resp4LU_L <- c(-1.6467178, -1.71574565, -1.75187842, -4.63752707, -0.28805273, -1.85820188, -1.22042155, -1.09866861, -2.76825158)
+Resp4LU_H <- c(-2.25851883, -3.19195594, -0.69002833, -0.60867025, -0.49310615, -0.84649393, -1.67568734, -1.09543746, -1.24130478)
+shapiro.test(Resp4HU_L)
+shapiro.test(Resp4HU_H)
+shapiro.test(Resp4LU_H)
+shapiro.test(Resp4LU_L)
 
-resp.model1 = lme(lgResp1 ~ pH*Location,  random = ~ 1|Jar/Coral, data = Resp1)
+resp.model14 = lmer(lgResp14 ~ pH*Location*Week + (1|Jar/Coral), data = Resp14)
+summary(resp.model14)
 
-aov1 = aov(resp.model1) 
-summary(aov1)
+Anova(resp.model14,test.statistic = "F") 
 
-
-############################################ same as above but with week 4
-
-Resp4=read.table("Resp_day29_manuscript.csv", header = TRUE)
-str(Resp4)
-head(Resp4)
-
-#making the Jar and Coral numbers facotrs and not integers
-Resp4$Jar <- as.factor(Resp4$Jar)
-Resp4$Coral <- as.factor(Resp4$Coral)
-str(Resp4)
-head(Resp4)
-
-#normalizaing the data
-attach(Resp4)
-abResp4=abs(umolghr)
-abResp4
-lgResp4=log(abResp4)
-lgResp4
-
-
-resp.model4 = lme(lgResp4 ~ pH*Location,  random = ~ 1|Jar/Coral, data = Resp4)
-
-aov4 = aov(resp.model4) 
-aov4
-summary(aov4)
-
+lsmeans(resp.model14, list(pairwise ~ pH*Location*Week), adjust = "tukey") 
